2ffb68bdf2f4910b14ff70700991bdf18956c5e5
Witold Mormul <w.mormul@gmail.com>
2019-03-30 18:23:57 +0100

    import from file

--- ga.py
+++ ga.py
@@ -1,5 +1,4 @@
-import numpy as np, random, operator, pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np, random, operator, pandas as pd, matplotlib.pyplot as plt
 
 
 class City:
@@ -50,6 +49,7 @@ def createRoute(cityList):
 
 def initialPopulation(popSize, cityList):
 	population = []
+
 	for i in range(0, popSize):
 		population.append(createRoute(cityList))
 	return population
@@ -67,19 +67,130 @@ def selection(popRanked, eliteSize):
 	df = pd.DataFrame(np.array(popRanked), columns=["Index", "Fitness"])
 	df['cum_sum'] = df.Fitness.cumsum()
 	df['cum_perc'] = 100 * df.cum_sum / df.Fitness.sum()
+
 	for i in range(0, eliteSize):
 		selectionResults.append(popRanked[i][0])
 	for i in range(0, len(popRanked) - eliteSize):
 		pick = 100 * random.random()
 		for i in range(0, len(popRanked)):
-			if pick <= df.iat[i,3]:
+			if pick <= df.iat[i, 3]:
 				selectionResults.append(popRanked[i][0])
 				break
 	return selectionResults
 
+
 def matingPool(population, selectionResults):
-	matingPool = []
+	matingpool = []
 	for i in range(0, len(selectionResults)):
 		index = selectionResults[i]
-		matingPool.append(population[index])
-	return matingPool
+		matingpool.append(population[index])
+	return matingpool
+
+
+def breed(parent1, parent2):
+	child = []
+	childP1 = []
+	childP2 = []
+
+	geneA = int(random.random() * len(parent1))
+	geneB = int(random.random() * len(parent1))
+
+	startGene = min(geneA, geneB)
+	endGene = max(geneA, geneB)
+
+	for i in range(startGene, endGene):
+		childP1.append(parent1[i])
+
+	childP2 = [item for item in parent2 if item not in childP1]
+
+	child = childP1 + childP2
+	return child
+
+
+def breedPopulation(matingpool, eliteSize):
+	children = []
+	length = len(matingpool) - eliteSize
+	pool = random.sample(matingpool, len(matingpool))
+
+	for i in range(0, eliteSize):
+		children.append(matingpool[i])
+
+	for i in range(0, length):
+		child = breed(pool[i], pool[len(matingpool) - i - 1])
+		children.append(child)
+	return children
+
+
+def mutate(individual, mutationRate):
+	for swapped in range(len(individual)):
+		if (random.random() < mutationRate):
+			swapWith = int(random.random() * len(individual))
+
+			city1 = individual[swapped]
+			city2 = individual[swapWith]
+
+			individual[swapped] = city2
+			individual[swapWith] = city1
+	return individual
+
+
+def mutatePopulation(population, mutationRate):
+	mutatedPop = []
+
+	for ind in range(0, len(population)):
+		mutatedInd = mutate(population[ind], mutationRate)
+		mutatedPop.append(mutatedInd)
+	return mutatedPop
+
+
+def nextGeneration(currentGen, eliteSize, mutationRate):
+	popRanked = rankRoutes(currentGen)
+	selectionResults = selection(popRanked, eliteSize)
+	matingpool = matingPool(currentGen, selectionResults)
+	children = breedPopulation(matingpool, eliteSize)
+	nextGeneration = mutatePopulation(children, mutationRate)
+	return nextGeneration
+
+
+def geneticAlgorithm(population, popSize, eliteSize, mutationRate, generations):
+	pop = initialPopulation(popSize, population)
+	print("Initial distance: " + str(1 / rankRoutes(pop)[0][1]))
+
+	for i in range(0, generations):
+		pop = nextGeneration(pop, eliteSize, mutationRate)
+		print("Step distance: " + str(1 / rankRoutes(pop)[0][1]))
+
+	print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
+	bestRouteIndex = rankRoutes(pop)[0][0]
+	bestRoute = pop[bestRouteIndex]
+	return bestRoute
+
+
+def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generations):
+	pop = initialPopulation(popSize, population)
+	progress = []
+	progress.append(1 / rankRoutes(pop)[0][1])
+
+	for i in range(0, generations):
+		pop = nextGeneration(pop, eliteSize, mutationRate)
+		progress.append(1 / rankRoutes(pop)[0][1])
+
+	plt.plot(progress)
+	plt.ylabel('Distance')
+	plt.xlabel('Generation')
+	plt.show()
+
+
+cityList = []
+
+f = open('data/hard_0.ttp')
+for line in f:
+	cityList.append(City(x=float(line.split()[1]), y=float(line.split()[2])))
+
+
+#for i in range(0, 25):
+#	cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
+
+geneticAlgorithmPlot(population=cityList, popSize=100, eliteSize=10, mutationRate=0.001, generations=500)
+
+f.close()
